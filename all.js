17ca16e68ffdfebcbc6a558ea0aad598bdd3af38
Ardalan Amini <ardalanamini22@gmail.com>
2022-06-19 17:01:26 +0430

    feat: Add `noir/all` config (#6)
    * feat: Add `noir/all` config
    
    * fix: package peerDependencies

---
+++ all.js
@@ -0,0 +1,12 @@
+"use strict";
+
+exports.extends = [
+  "eslint:recommended",
+  "noir",
+  "plugin:@typescript-eslint/recommended",
+  "noir/typescript",
+];
+
+exports.parser = "@typescript-eslint/parser";
+
+exports.plugins = ["@typescript-eslint"];
