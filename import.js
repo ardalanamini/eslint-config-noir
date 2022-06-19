cdeda0490f4867920bfeae12735b5bcd0622597e
Ardalan Amini <ardalanamini22@gmail.com>
2022-06-19 17:50:56 +0430

    feat: Add `noir/import` config (#7)
    * deps(Dev): Add eslint-plugin-import version 2.26.0
    
    * feat: Add `noir/import` config
    
    * chore: Update README.md

---
+++ import.js
@@ -0,0 +1,37 @@
+"use strict";
+
+exports.rules = {
+  /**
+   * Static analysis ---------------------------------------------------------------------------------------------------
+   * @see https://github.com/import-js/eslint-plugin-import#static-analysis
+   */
+
+  "import/no-absolute-path"          : "error",
+  "import/no-cycle"                  : "error",
+  "import/no-self-import"            : "error",
+  "import/no-relative-parent-imports": "error",
+
+  /**
+   * Helpful warnings --------------------------------------------------------------------------------------------------
+   * @see https://github.com/import-js/eslint-plugin-import#helpful-warnings
+   */
+
+  "import/no-named-as-default"       : "error",
+  "import/no-named-as-default-member": "error",
+  "import/no-deprecated"             : "error",
+
+  /**
+   * Module systems ----------------------------------------------------------------------------------------------------
+   * @see https://github.com/import-js/eslint-plugin-import#module-systems
+   */
+
+  /**
+   * Style guide -------------------------------------------------------------------------------------------------------
+   * @see https://github.com/import-js/eslint-plugin-import#style-guide
+   */
+
+  "import/first"               : "error",
+  "import/exports-last"        : "error",
+  "import/no-duplicates"       : ["error", { considerQueryString: true }],
+  "import/newline-after-import": ["error", { count: 1 }],
+};
